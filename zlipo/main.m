f6d4b19805277744ec18c764db336e61d6c451d6
sunyanguo <sunyanguo@lvmama.com>
2018-07-27 10:39:48 +0800

    Initial Commit

---
+++ zlipo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  zlipo
+//
+//  Created by yanguo sun on 2018/7/27.
+//  Copyright © 2018 Lvmama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
